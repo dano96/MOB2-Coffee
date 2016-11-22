d63670094135e8d1907c66d73aeb1a58c55bfb05
LeaTeMaiharoa <rebbalingur@gmail.com>
2016-11-22 18:09:40 +0100

    dialog

--- app/src/main/java/com/example/android/mob2_assignment/activities/CoffeeActivity.java
+++ app/src/main/java/com/example/android/mob2_assignment/activities/CoffeeActivity.java
@@ -1,14 +1,20 @@
 package com.example.android.mob2_assignment.activities;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
 import com.example.android.mob2_assignment.BackgroundConnection;
 import com.example.android.mob2_assignment.Preset;
 import com.example.android.mob2_assignment.R;
@@ -21,6 +27,12 @@ public class CoffeeActivity extends AppCompatActivity implements View.OnClickLis
 
     BluetoothHandler mHandler;
     BluetoothHandler.ConnectedThread readWrite;
+    RadioGroup radioCupGroup;
+    EditText hoursText;
+    EditText minText;
+    RadioGroup timeToMakeRadio;
+    EditText nameOfPreset;
+    ArrayAdapter<Preset> itemsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +47,7 @@ public class CoffeeActivity extends AppCompatActivity implements View.OnClickLis
 
         arrayList.addAll(Arrays.asList(first, second, third));
 
-        ArrayAdapter<Preset> itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
 
         ImageButton oneCupButton = (ImageButton) findViewById(R.id.oneCupButton);
         ImageButton twoCupsButton = (ImageButton) findViewById(R.id.twoCupsButton);
@@ -77,7 +89,39 @@ public class CoffeeActivity extends AppCompatActivity implements View.OnClickLis
                 readWrite.write("0".getBytes());
                 break;
             case R.id.setPresetButton:
+                View dialogLayout = getLayoutInflater().inflate(R.layout.preset_dialog, null);
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Create a preset");
+                builder.setView(dialogLayout);
+                builder.setCancelable(false);
 
+                nameOfPreset = (EditText) dialogLayout.findViewById(R.id.nameOfPreset);
+                radioCupGroup = (RadioGroup) dialogLayout.findViewById(R.id.radiocup);
+                hoursText = (EditText) dialogLayout.findViewById(R.id.hour);
+                minText = (EditText) dialogLayout.findViewById(R.id.minutes);
+                timeToMakeRadio = (RadioGroup) dialogLayout.findViewById(R.id.time_to_makeradio);
+
+                builder.setNegativeButton("Save", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        int selectedRadioButton = radioCupGroup.getCheckedRadioButtonId();
+                        RadioButton button = (RadioButton) findViewById(R.id.radioButton4);
+                        String name = nameOfPreset.getText().toString();
+                        int cup = selectedRadioButton == R.id.radioButton4? 1 : 2;
+                        Date somedate = new Date();
+                        Preset current = new Preset(name, cup, somedate);
+                        itemsAdapter.add(current);
+
+                    }
+                });
+                builder.setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
                 break;
             default:
                 break;
