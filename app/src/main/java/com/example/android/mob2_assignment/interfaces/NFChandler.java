312573a6592c8faa9f3b5ed495d334d6e1d8ab48
Atanas Latinov <atanas.latinov@yahoo.com>
2016-11-25 15:04:51 +0100

    undo nfchandler deletion

---
+++ app/src/main/java/com/example/android/mob2_assignment/interfaces/NFChandler.java
@@ -0,0 +1,41 @@
+package com.example.android.mob2_assignment.interfaces;
+
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.os.Parcelable;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+
+public class NFChandler {
+
+    public static String readTag(Intent intent) {
+        NdefMessage[] msgs;
+        if (intent.getAction().equals(NfcAdapter.ACTION_NDEF_DISCOVERED)) {
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (rawMsgs != null) {
+                msgs = new NdefMessage[rawMsgs.length];
+                for (int i = 0; i < rawMsgs.length; i++) {
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+                NdefRecord[] records = msgs[0].getRecords();
+                for (NdefRecord ndefRecord : records) {
+                    if (ndefRecord.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
+                        byte[] payload = ndefRecord.getPayload();
+                        String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
+                        int languageCodeLength = payload[0] & 0063;
+                        try {
+                            String tagText = new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+                            return tagText;
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
+}
